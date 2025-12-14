23203587200671e980c7583d85d4179c4860ecee
Aradhya Bhagwat <145675303+aradhya0507@users.noreply.github.com>
2025-12-14 09:17:25 +0530

    Implemented bubble sort for array

--- task2.java
+++ task2.java
@@ -14,8 +14,31 @@ public class task2
                 System.out.println("Enter element " + (i + 1) + ": ");
                 arr[i] = sc.nextInt();
             }
+            arr = sortArray(arr);
+            System.out.println("Sorted Array : ");
+            for (int i = 0; i < arr.length; i++) 
+            {
+                System.out.print(arr[i] + " ");
+            }
             sc.close();
         }
     }
-    
+
+    //Impelemented Bubble Sort Algorithm
+    public static int[] sortArray(int[] arr) 
+    {
+        for (int i = 0; i < arr.length - 1; i++) 
+        {
+            for (int j = 0; j < arr.length - i - 1; j++) 
+            {
+                if (arr[j] > arr[j + 1]) 
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        return arr;
+    } 
 }
